d281095c31167dcd4a9beecb8e6374f079a2ca58
nazar301 <nazar.basistyy@gmail.com>
2022-02-09 18:51:30 -0500

    register update

--- users/user.js
+++ users/user.js
@@ -1,14 +1,11 @@
 const mongoose = require('../db/connection')
 
 const userSchema = new mongoose.Schema({
-<<<<<<< HEAD
     email: {
         type:String,
         unique: true,
         required: true
     },
-=======
->>>>>>> 4d37e4e (rebase)
     username: {
         type: String,
         unique: true,
