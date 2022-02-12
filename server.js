6346a111daad86a7b6dea658f158e8a2bf73c8b7
nazar301 <nazar.basistyy@gmail.com>
2022-02-12 12:51:31 -0500

    working registration

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const noteController = require('./controllers/noteController')
 // const Note = require('./models/note')
 const userRoute = require('./controllers/sessions')
 
+
 // Bring in cors to communicate across ports
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use('/user', userRoute)
+app.use('/session', userRoute)
 app.use('/notes', noteController)
 
 app.listen(PORT, () => {
