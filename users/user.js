6ed9489ba6def380e55a9b9880d3d9a9bd18ff73
nazar301 <nazar.basistyy@gmail.com>
2022-02-13 14:34:37 -0500

    rebase

--- users/user.js
+++ users/user.js
@@ -1,11 +1,14 @@
 const mongoose = require('../db/connection')
 
 const userSchema = new mongoose.Schema({
+<<<<<<< HEAD
     email: {
         type:String,
         unique: true,
         required: true
     },
+=======
+>>>>>>> 4d37e4e (rebase)
     username: {
         type: String,
         unique: true,
